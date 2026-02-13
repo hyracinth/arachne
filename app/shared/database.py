dc79e3e69c82042677ce11b939a1d9339df64b69
hyracinth <jonahlou@gmail.com>
2026-02-13 12:52:03 -0500

    database - removed fstring for parameterized query

--- app/shared/database.py
+++ app/shared/database.py
@@ -1,9 +1,23 @@
+import os
+import ipaddress
 import psycopg2
 from psycopg2.extras import RealDictCursor
-import os
 from dotenv import load_dotenv
 
 load_dotenv()
+
+def validate_ip(ip):
+    try:
+        ipaddress.ip_address(ip)
+        return True
+    except ValueError:
+        return False
+
+def clean_input(value, max_len=255):
+    if value is None:
+        return ""
+    data = data[:max_len]
+    return str(char for char in data if char.isprintable()).strip()
 
 class ArachneDB:
     def __init__(self):
@@ -17,13 +31,25 @@ class ArachneDB:
         return conn
     
     def add_attack(self, ip, user=None, password=None, city=None, country=None, latitude=None, longitude=None):
-        query = f"""
-            INSERT INTO attacks (ip_address, username, password, city, country, latitude, longitude)
-            VALUES ('{ip}', '{user}', '{password}', '{city}', '{country}', '{latitude}', '{longitude}')
-        """
-        print(query)
+        if not validate_ip(ip):
+            raise ValueError(f"Invalid IP address: {ip}")
+        
+        # TODO find better way to do this?
+        user = clean_input(user)
+        password = clean_input(password)
+        city = clean_input(city)
+        country = clean_input(country)
+        latitude = clean_input(latitude, max_len=10)
+        longitude = clean_input(longitude, max_len=10)
+
+        # Use parameterized query to prevent SQL injection
+        query = """
+                INSERT INTO attacks (ip_address, username, password, city, country, latitude, longitude)
+                VALUES (%s, %s, %s, %s, %s, %s, %s)
+                """
+
         conn = self._get_conn()
         with conn:
             with conn.cursor() as cur:
-                cur.execute(query)
+                cur.execute(query, (ip, user, password, city, country, latitude, longitude))
             conn.commit()
