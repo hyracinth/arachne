37bdb00d6a43362af1fc5d42b6e29760ceed0691
hyracinth <jonahlou@gmail.com>
2026-02-23 20:40:30 -0500

    #4 dashboard - revamped ui. changed slider to hours duration. Map elements pulse. Aggregate attacks to minimize layers

--- apps/dashboard/app.py
+++ apps/dashboard/app.py
@@ -30,17 +30,85 @@ def get_bezier_path(start, end, height=0.3, points=20):
         
     return path_points
 
-app_ui = ui.page_fluid(
-    ui.panel_title("Arachne Monitor"),
+app_ui = ui.page_fillable(
+    ui.head_content(
+        ui.tags.script("""
+            document.documentElement.setAttribute('data-bs-theme', 'dark');
+        """),
+        # Fallback: Target by stroke color because ipyleaflet seems to be ignoring class names
+        # or applying them to parent groups, breaking direct css styling
+        ui.tags.style("""
+            .card { border: 1px solid #2a2a2a; background-color: #121212; }
 
-    ui.navset_card_pill(
-        ui.nav_panel("Live Map", output_widget("map")),
-        ui.nav_panel("Data Grid", ui.output_data_frame("attack_table"))
+            path[stroke='#28f049']  {
+                animation: linePulse 4s ease-in-out infinite !important;
+            }
+            @keyframes linePulse {
+                0% { opacity: 0.4; }
+                50% { opacity: 0.8; }
+                100% { opacity: 0.4; }
+            }
+
+            path[stroke='red'] {
+                animation: beacon 3s ease-out infinite !important;
+                transform-origin: center;
+                transform-box: fill-box;
+            }
+            @keyframes beacon {
+                0% { transform: scale(3); opacity: 0.1; }
+                100% { transform: scale(1); opacity: 0.7; }
+            }
+
+        """)
+    ),
+    ui.layout_columns(
+        ui.card(
+            ui.card_header("project: arachne"),
+            ui.p(output_widget("map")),
+        ),
+        ui.card(
+            ui.card(
+                ui.card_header("scan duration"), 
+                ui.input_slider(id="duration_slider", label="", min=1, max=168, value=24),
+            ),
+            ui.card(
+                ui.card_header("total threats"), 
+                ui.output_text_verbatim("duration_slider_value"),),
+            ui.card(
+                ui.card_header("attack logs"), 
+                ui.p("[HH:mm:ss] Logged attack from XYZ..."),
+                ui.p("[HH:mm:ss] Logged attack from ABC..."),
+                ui.p("[HH:mm:ss] Logged attack from AAA..."),
+                ui.p("[HH:mm:ss] Logged attack from BBB..."),
+            ),
+        ),
+        col_widths=(8,4)
     )
-    
 )
 
+# app_ui = ui.page_fluid(
+#     ui.panel_title("Arachne Monitor"),
+#     ui.input_dark_mode(),
+#     ui.navset_card_pill(
+#         ui.nav_panel("Live Map", output_widget("map")),
+#         ui.nav_panel("Data Grid", ui.output_data_frame("attack_table"))
+#     )
+# )
+
 def server(input, output, session):
+    @render.text
+    def duration_slider_value():
+        return f"{len(enriched_attacks_map())}"
+    
+    @reactive.calc
+    def enriched_attacks_map():
+        try:
+            data = db.get_enriched(input.duration_slider())
+            return data if data else []
+        except Exception as e:
+            print(f"[ERROR] Failed to retrieve enriched data: {e}")
+            return []
+    
     @render.data_frame
     def attack_table():
         try:
@@ -57,23 +125,21 @@ def server(input, output, session):
         return render.DataTable(df)
 
 
-    @render_widget  
+    @render_widget
     def map():
         m = Map(
                 basemap=basemaps.CartoDB.DarkMatter,
                 # basemap=basemaps.Esri.WorldImagery,
                 # basemap=basemaps.NASAGIBS.ViirsEarthAtNight2012,
+                # basemap=basemaps.Stadia.AlidadeSmoothDark,
                 scroll_wheel_zoom=True)
         if len(m.layers) > 1:
             m.layers = m.layers[:1]
 
-        try:
-            attacks = db.get_enriched()
-        except Exception as e:
-            print(f'Error fetching attacks: {e}')
-            return m
-        
-        print(f'Fetched {len(attacks)} enriched attacks for map display')
+        attacks = enriched_attacks_map()
+
+        aggregated_attacks = {}
+
         min_lat = HONEYPOT_COORDS[0]
         max_lat = HONEYPOT_COORDS[0]
         min_lon = HONEYPOT_COORDS[1]
@@ -89,36 +155,43 @@ def server(input, output, session):
                 if lon > max_lon: max_lon = lon
                 curved_points = get_bezier_path(start=(lat, lon), end=HONEYPOT_COORDS, points=10)
 
-                # Neat animation but not sure if I want this..
-                # line = AntPath(
-                #     hardware_acceleration=True,
-                #     locations=curved_points,
-                #     dash_array=[1, 10],
-                #     delay=1000,
-                #     color="#19b158",
-                #     pulse_color='#ffffff',
-                #     opacity=0.6,
-                #     weight=2,
-                # )
+                if (lat, lon) not in aggregated_attacks:
+                        aggregated_attacks[(lat, lon)] = {
+                            "points": curved_points,
+                            "count": 1,
+                        }
+                else:
+                    aggregated_attacks[(lat,lon)]["count"] = aggregated_attacks[(lat,lon)]["count"] + 1
 
-                line = Polyline(
-                    locations=curved_points,
-                    color="#14bb4c",
-                    weight=1,
-                    opacity=0.4,
-                    fill=False,
-                )
-                m.add_layer(line)
-
-            target = CircleMarker(
-                location=HONEYPOT_COORDS,
-                radius=5,
-                color="red",
-                fill_color="red",
+        for lat, lon in aggregated_attacks:
+            source = CircleMarker(
+                location=(lat, lon),
+                radius=2,
+                color="orange",
+                fill_color="orange",
                 fill_opacity=0.2,
                 weight=1
             )
-            m.add_layer(target)
+
+            line = Polyline(
+                locations=aggregated_attacks[(lat, lon)]["points"],
+                color="#28f049",
+                weight=min(aggregated_attacks[(lat, lon)]["count"], 3),
+                opacity=0.4,
+                fill=False,
+            )
+            m.add_layer(line)
+            m.add_layer(source)
+
+        target = CircleMarker(
+            location=HONEYPOT_COORDS,
+            radius=5,
+            color="red",
+            fill_color="red",
+            fill_opacity=0.8,
+            weight=1,
+        )
+        m.add_layer(target)
 
         # 60% off the bottom to shift view window up
         min_lat = min_lat + (abs(max_lat - min_lat) * 0.6)
